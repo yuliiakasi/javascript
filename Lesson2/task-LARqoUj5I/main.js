61f9b595c9743bfdf995c12373d46008a6512898
yuliiakasi <yuliia.kasi@gmail.com>
2025-02-09 22:51:15 +0100

    task 1

---
+++ Lesson2/task-LARqoUj5I/main.js
@@ -0,0 +1,17 @@
+let book1 = {
+  title: 'Book First',
+  pageCount: 324,
+  genre: 'fantasy'
+}
+
+let book2 = {
+  title: 'Book Second',
+  pageCount: 123,
+  genre: 'crime'
+}
+
+let book3 = {
+  title: 'Book Third',
+  pageCount: 43,
+  genre: 'biography'
+}
