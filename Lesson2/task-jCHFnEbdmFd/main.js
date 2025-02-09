1856248ac6431b9e95e452697a31e00b4332fa8a
yuliiakasi <yuliia.kasi@gmail.com>
2025-02-09 23:30:58 +0100

    task4

---
+++ Lesson2/task-jCHFnEbdmFd/main.js
@@ -0,0 +1,25 @@
+let users = [
+  {name: 'name1', username: 'username1', password:'password1'},
+  {name: 'name2', username: 'username2', password:'password2'},
+  {name: 'name3', username: 'username3', password:'password3'},
+  {name: 'name4', username: 'username4', password:'password4'},
+  {name: 'name5', username: 'username5', password:'password5'},
+  {name: 'name6', username: 'username6', password:'password6'},
+  {name: 'name7', username: 'username7', password:'password7'},
+  {name: 'name8', username: 'username8', password:'password8'},
+  {name: 'nam9', username: 'username9', password:'password9'},
+  {name: 'name10', username: 'username10', password:'password10'},
+];
+
+console.log(users[0]["password"]);
+console.log(users[1]["password"]);
+console.log(users[2]["password"]);
+console.log(users[3]["password"]);
+console.log(users[4]["password"]);
+console.log(users[5]["password"]);
+console.log(users[6]["password"]);
+console.log(users[7]["password"]);
+console.log(users[8]["password"]);
+console.log(users[9]["password"]);
+
+
