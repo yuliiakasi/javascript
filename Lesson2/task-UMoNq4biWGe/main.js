ac7711a6a5c08fc1c3ba6d8ac1d5df63eabcda9c
yuliiakasi <yuliia.kasi@gmail.com>
2025-02-10 00:21:54 +0100

    task8

---
+++ Lesson2/task-UMoNq4biWGe/main.js
@@ -0,0 +1,13 @@
+let day = 12;
+
+if (day <= 15 && day > 0) {
+  console.log('Перша декада');
+}
+else if (day>15 && day<=31) {
+  console.log('Друга декада');
+}
+else {
+  console.log('Такої дати не існує');
+}
+
+
