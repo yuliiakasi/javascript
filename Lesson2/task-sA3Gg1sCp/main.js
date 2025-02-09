219fb884ba59d005fd7360e1b7d6bf3fb8d6b873
yuliiakasi <yuliia.kasi@gmail.com>
2025-02-09 23:15:47 +0100

    task3

---
+++ Lesson2/task-sA3Gg1sCp/main.js
@@ -0,0 +1,30 @@
+let book1 = {
+  title: 'Book First',
+  pageCount: 324,
+  genre: 'fantasy',
+  authors: [
+    {name: 'Svitlana', age: 46},
+    {name: 'Ekaterina', age: 37},
+  ],
+}
+
+let book2 = {
+  title: 'Book Second',
+  pageCount: 123,
+  genre: 'crime',
+  authors: [
+    {name: 'Nikolay', age: 30},
+    {name: 'Viktor', age: 52},
+  ],
+}
+
+let book3 = {
+  title: 'Book Third',
+  pageCount: 43,
+  genre: 'biography',
+  authors: [
+    {name: 'Oleg', age: 21},
+    {name: 'Svitlana', age: 46},
+  ],
+}
+
