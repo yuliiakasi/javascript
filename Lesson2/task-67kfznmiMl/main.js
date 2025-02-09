65821e6092b65e4d0acdd779115fd9900181c114
yuliiakasi <yuliia.kasi@gmail.com>
2025-02-09 22:45:34 +0100

    task1 changes

---
+++ Lesson2/task-67kfznmiMl/main.js
@@ -0,0 +1,13 @@
+let arr = [1243, 3498, [], false, true, 0.5, 'name', 'hello', 'print', 453]
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9]);
+
