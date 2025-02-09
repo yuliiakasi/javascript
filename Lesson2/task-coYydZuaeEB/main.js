88588aea1375743ebb457d4fcbf9ee9a00a7e6ad
yuliiakasi <yuliia.kasi@gmail.com>
2025-02-09 23:52:12 +0100

    task5

---
+++ Lesson2/task-coYydZuaeEB/main.js
@@ -0,0 +1,11 @@
+let weekTemperature = [
+  {day: 'Monday', morning: 12, afternoon: 16, evening: 14},
+  {day: 'Tuesday', morning: 15, afternoon: 18, evening: 15},
+  {day: 'Wednesday', morning: 13, afternoon: 16, evening: 11},
+  {day: 'Thursday', morning: 12, afternoon: 20, evening: 16},
+  {day: 'Friday', morning: 15, afternoon: 18, evening: 13},
+  {day: 'Saturday', morning: 17, afternoon: 23, evening: 16},
+  {day: 'Sunday', morning: 14, afternoon: 18, evening: 12},
+]
+
+// console.log(weekTemperature);
