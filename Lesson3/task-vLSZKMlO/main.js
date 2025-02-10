ae4b24c098182ef9bdfe012b0bab7e5964874b68
yuliiakasi <yuliia.kasi@gmail.com>
2025-02-10 22:14:30 +0100

    task5

---
+++ Lesson3/task-vLSZKMlO/main.js
@@ -0,0 +1,10 @@
+let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+
+for (let listOfItem of listOfItems) {
+  document.write(`
+    <ul>
+      <li>${listOfItem}</li>
+    </ul>
+  `);
+
+}
