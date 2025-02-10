8362e099ff1c3fc592a3637413e6cbee74f5b8ce
yuliiakasi <yuliia.kasi@gmail.com>
2025-02-10 19:11:05 +0100

    task9

---
+++ Lesson2/task-KzrtqyQ/main.js
@@ -0,0 +1,36 @@
+let field = prompt('Enter the day of week from 1 to 7');
+
+switch (field) {
+  case '1':
+    console.log('Monday');
+    break;
+
+  case '2':
+    console.log('Tuesday');
+    break;
+
+  case '3':
+    console.log('Wednesday');
+    break;
+
+  case '4':
+    console.log('Thursday');
+    break;
+
+  case '5':
+    console.log('Friday');
+    break;
+
+  case '6':
+    console.log('Saturday');
+    break;
+
+  case '7':
+    console.log('Sunday');
+    break;
+
+  default:
+    console.log('You entered the wrong day of the week')
+
+
+}
