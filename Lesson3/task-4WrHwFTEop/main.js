4b0db262a2fcd278e5c180336440bfd48d8473d0
yuliiakasi <yuliia.kasi@gmail.com>
2025-02-10 22:45:40 +0100

    task7

---
+++ Lesson3/task-4WrHwFTEop/main.js
@@ -0,0 +1,35 @@
+let users = [
+  {name: 'vasya', age: 31, status: false},
+  {name: 'petya', age: 30, status: true},
+  {name: 'kolya', age: 29, status: true},
+  {name: 'olya', age: 28, status: false},
+  {name: 'max', age: 30, status: true},
+  {name: 'anya', age: 31, status: false},
+  {name: 'oleg', age: 28, status: false},
+  {name: 'andrey', age: 29, status: true},
+  {name: 'masha', age: 30, status: true},
+  {name: 'olya', age: 31, status: false},
+  {name: 'max', age: 31, status: true}
+];
+
+for (const user of users) {
+  if (user["status"]===true) {
+    console.log(user);
+  }
+}
+
+console.log('')
+
+for (const user of users) {
+  if (user["status"]===false) {
+    console.log(user);
+  }
+}
+
+console.log('')
+
+for (const user of users) {
+  if (user["age"]>30){
+    console.log(user);
+  }
+}
