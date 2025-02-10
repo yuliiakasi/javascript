a99531cf520e9640b9febef3d05776e40a6bde9d
yuliiakasi <yuliia.kasi@gmail.com>
2025-02-10 18:57:00 +0100

    task7

---
+++ Lesson2/task-3ckURgvs/main.js
@@ -0,0 +1,18 @@
+let time = 15;
+
+
+if (time<=15) {
+  console.log('Перша четверть');
+}
+
+else if (time>15 && time<=30) {
+  console.log('Друга четверть');
+}
+
+else if (time>30 && time<=45) {
+  console.log('Третя четверть')
+}
+
+else if (time>45 && time<=59) {
+  console.log('Четверта четверть');
+}
