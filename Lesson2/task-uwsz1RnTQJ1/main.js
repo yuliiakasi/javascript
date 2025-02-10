1c50659dfac6c39cb1184d2d0f9021b9aeb5fee4
yuliiakasi <yuliia.kasi@gmail.com>
2025-02-10 19:27:54 +0100

    task10

---
+++ Lesson2/task-uwsz1RnTQJ1/main.js
@@ -0,0 +1,20 @@
+let a = +prompt('Enter any number for a');
+let b = +prompt('Ebter any number for b');
+
+if (a>b) {
+  console.log(a);
+}
+
+else if (a<b) {
+  console.log(b);
+}
+
+else if (a===b) {
+  console.log('You entered the same values');
+}
+
+else {
+  console.log('You entered a value in an invalid format');
+}
+
+
