d53aa4df58c8fe6f5efdcd6207b532a8758496b4
yuliiakasi <yuliia.kasi@gmail.com>
2025-02-17 22:45:04 +0100

    task6

---
+++ Lesson5/task-hOL6126/main.js
@@ -0,0 +1,13 @@
+//- створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+
+let createList = (text) => {
+  document.write(`
+        <ul>
+            <li>${text}</li>
+            <li>${text}</li>
+            <li>${text}</li>
+        </ul>
+`);
+}
+
+createList('hallo');
