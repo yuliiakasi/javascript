d8e7acbad52de09dd98355da89390a6f37775a4b
yuliiakasi <yuliia.kasi@gmail.com>
2025-02-18 21:30:48 +0100

    task1

---
+++ Lesson6/task-dFeorS3m7u/main.js
@@ -0,0 +1,10 @@
+//- Знайти та вивести довижину настипних стрінгових значень
+//     'hello world', 'lorem ipsum', 'javascript is cool'
+
+let str1 = 'hello world';
+let str2 = 'lorem ipsum';
+let str3 = 'javascript is cool';
+
+console.log(str1.length);
+console.log(str2.length);
+console.log(str3.length);
