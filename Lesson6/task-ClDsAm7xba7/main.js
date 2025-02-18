448801673085bf26626e6fbdeac21cd41b93b496
yuliiakasi <yuliia.kasi@gmail.com>
2025-02-18 21:42:09 +0100

    task3

---
+++ Lesson6/task-ClDsAm7xba7/main.js
@@ -0,0 +1,10 @@
+//- Перевести до нижнього регістру настипні стрінгові значення
+//       'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+
+let str1 =  'HELLO WORLD';
+let str2 = 'LOREM IPSUM';
+let str3 = 'JAVASCRIPT IS COOL';
+
+const s1 = str1.toLowerCase();
+const s2 = str2.toLowerCase();
+const s3 = str3.toLowerCase();
