fa265a8750ea9b319936f403bb870884e3052cf7
yuliiakasi <yuliia.kasi@gmail.com>
2025-03-04 02:42:50 +0100

    task5

---
+++ Lesson7/task-8abtVjRv/main.js
@@ -0,0 +1,27 @@
+//- Взяти масив (Client [] з попереднього завдання).Відсортувати його по кількості товарів в полі order по зростанню. (sort)
+
+function Client(id, name, surname, email, phone, order) {
+  this.id = id;
+  this.name = name;
+  this.surname = surname;
+  this.email = email;
+  this.phone = phone;
+  this.order = order;
+}
+
+let clients = [
+  new Client(1, 'Vasya', 'Kotov', 'vasya@gmail.com', '+23523469', ['tablet', 'tv']),
+  new Client(2, 'Michael', 'Brown', 'michael@gmail.com', '+4575366', ['tablet', 'tv', 'phone']),
+  new Client(3, 'Mathew', 'Thompson', 'mathew@gmail.com', '+01240524', ['tv']),
+  new Client(4, 'Robert', 'Rios', 'rios@gmail.com', '+08672621', ['tablet', 'headphones', 'charger', 'dishwasher', 'powerbank']),
+  new Client(5, 'Robert', 'Rowe', 'rowe@gmail.com', '+53464726', ['phone']),
+  new Client(6, 'Alvin', 'Haynes', 'haynes@gmail.com', '+1323267', ['speaker', 'microphone']),
+  new Client(7, 'Mildred', 'Webster', 'webster@gmail.com', '+457414', ['motherboard']),
+  new Client(8, 'Ashley', 'Jenkins', 'jenkins@gmail.com', '+43205641', ['tablet', 'tv', 'cooler', 'receiver']),
+  new Client(9, 'Phyllis', 'Martin', 'martin@gmail.com', '+23523469', ['phone']),
+  new Client(10, 'Brenda', 'Garcia', 'garcia@gmail.com', '+3489636', ['projector', 'fridge', 'stove', 'soundbar']),
+];
+
+
+const result = clients.sort((a, b) => a.order.length - b.order.length);
+
